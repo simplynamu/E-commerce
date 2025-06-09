38d4bb9f1ce33ff05afda11cec30df1e24f4a52e
simplynamu <124071197+simplynamu@users.noreply.github.com>
2025-06-09 13:56:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -40,8 +40,9 @@ app.use('/images', express.static('upload/images'))
 app.post('/upload', upload.single(`product`), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
-        // image_url: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+        //image_url: `http://localhost:${port}/images/${req.file.filename}`
+        //image_url: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+        image_url: `https://e-commerce-0v1w.onrender.com/images/${req.file.filename}`
     });
 });
 
